ea63e2b0f139501a6c800052159916025b1d3cf6
JAVI <javvincos@alu.edu.gva.es>
2023-11-13 18:53:56 +0100

    cambios en ejericio 7 Fibonacci

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -4,24 +4,21 @@
  * @returns vector con la serie de Fibonacci.
  * @author: Sustituye tu nombre y apellidos aquí
  */
-function fibonacci(n)
-{
-    let vector=new Array(); // Creamos un vector para alamcenar el cálculo
-    let i=0;
-    if(n<0) return vector;
-
+ function fibonacci(n) {
+    let vector = [0, 1]; // Inicializamos el vector con los valores iniciales
+    let i = 2;
+    if (n < 0) return vector;
+  
     do {
-
-        console.log(i);
-        if (i == 1 || i == 0) { vector[i] = 1; i++; continue; };
-        vector[i] = vector[i - 2] + vector[i - 1];
-        console.log("v" + vector[i - 1]);
-        i++;
-
+      console.log(i);
+      vector[i] = vector[i - 2] + vector[i - 1];
+      console.log("v" + vector[i - 1]);
+      i++;
     } while (i <= n);
-    
+  
     return vector; // Retorno de la función
-}
+  }
+  
 
 /**
  * Aplicación principal Ejercicio 7
