bf618195d4b7ebb6821d46df40081f09ead671ae
JAVI <javvincos@alu.edu.gva.es>
2023-11-13 18:55:37 +0100

    correcion ejec 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -4,20 +4,25 @@
  * @returns vector con la serie de Fibonacci.
  * @author: Sustituye tu nombre y apellidos aquí
  */
- function fibonacci(n) {
-    let vector = [0, 1]; // Inicializamos el vector con los valores iniciales
-    let i = 2;
-    if (n < 0) return vector;
-  
-    do {
-      console.log(i);
-      vector[i] = vector[i - 2] + vector[i - 1];
-      console.log("v" + vector[i - 1]);
-      i++;
-    } while (i <= n);
-  
-    return vector; // Retorno de la función
-  }
+ function fibonacci(n)
+ {
+     let vector=new Array(); // Creamos un vector para alamcenar el cálculo
+     let i=0;
+     if (n <= 0){return vector}
+     do{
+         
+         console.log(i);
+         if (i == 1 || i == 0){vector[i] = 1;i++;continue;};
+         vector[i]= vector[i-2]+vector[i-1];
+         console.log("v"+vector[i-1]);
+         i++;
+     }while(i<n)
+     
+     // ESCRIBE TU CÓDIGO AQUÍ
+     
+     
+     return vector; // Retorno de la función
+ }
   
 
 /**
