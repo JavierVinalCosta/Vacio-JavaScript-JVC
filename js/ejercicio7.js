758b44b91632c2cd3f511f3122d656c94d03bd5c
JAVI <javvincos@alu.edu.gva.es>
2023-11-13 17:15:43 +0100

    Completado ejercicio 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -7,10 +7,18 @@
 function fibonacci(n)
 {
     let vector=new Array(); // Creamos un vector para alamcenar el cálculo
-    
-    
-    // ESCRIBE TU CÓDIGO AQUÍ
-    
+    let i=0;
+    if(n<0) return vector;
+
+    do {
+
+        console.log(i);
+        if (i == 1 || i == 0) { vector[i] = 1; i++; continue; };
+        vector[i] = vector[i - 2] + vector[i - 1];
+        console.log("v" + vector[i - 1]);
+        i++;
+
+    } while (i <= n);
     
     return vector; // Retorno de la función
 }
